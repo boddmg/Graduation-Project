bbf60cf5080f6d3ec98f61489fcdaa75fa7b4ee3
boddmg <boddmg@gmail.com>
2015-03-30 01:09:31 +0800

    Add the batch spliter in the Preprocessor moudle.

--- program/cad60-cnn/preprocess.py
+++ program/cad60-cnn/preprocess.py
@@ -34,18 +34,45 @@ class Shuffle(Preprocessor):
         src_labels = src_labels[indices]
         return src_data, src_labels
 
-class GetBatch(Preprocessor):
+class SplitIntoBatches(Preprocessor):
     def __init__(self, batch_size = 1, stride = 1):
         self.batch_size = batch_size
         self.stride = stride
 
+    def getSplitPoint(self, src_labels):
+        label_split_point = [0]
+        last_label = src_labels[0]
+        for i in range(len(src_labels)):
+            if src_labels[i] != last_label:
+                last_label = src_labels[i]
+                label_split_point.append(i)
+        return label_split_point
+
     def run(self, src_data, src_labels):
+        data_point_size = src_data.shape[2]
+        split_point = self.getSplitPoint(src_labels)
+        batch_index = []
+
+        for i in range(len(split_point) - 1):
+            start_point = split_point[i]
+            end_point = split_point[i+1]
+            for j in range(start_point, end_point, self.stride):
+                if j+self.batch_size <= end_point:
+                    batch_index.append([j,j+self.batch_size])
+        dst_data = numpy.zeros([len(batch_index), self.batch_size,data_point_size],  dtype=numpy.float32)
+        dst_labels = numpy.zeros([len(batch_index)], dtype=numpy.uint8)
+
+        index = 0
+        for i in batch_index:
+            dst_data[index] = src_data[i[0]:i[1]].reshape(self.batch_size, data_point_size)
+            dst_labels[index] = src_labels[i[0]]
+            index += 1
         return src_data, src_labels
 
 
 def main():
     src_data, src_labels, temp = CAD60(batch_size=1, data_type="train").get_data()
-    src_data, src_labels = PreprocessorList([Shuffle()]).run(src_data,src_labels)
+    src_data, src_labels = PreprocessorList([SplitIntoBatches(10, 5),Shuffle()]).run(src_data,src_labels)
     print src_data, src_labels
 
 if __name__ == '__main__':
