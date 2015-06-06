50dea3f93939b5f771c383a9fc5b2f8b65975130
boddmg <boddmg@gmail.com>
2015-06-07 02:10:20 +0800

    Add the zmq CS model.

--- program/cad60-cnn/experiment/demo/cad60_demo.py
+++ program/cad60-cnn/experiment/demo/cad60_demo.py
@@ -8,18 +8,18 @@ from blocks.bricks.conv import ConvolutionalLayer, ConvolutionalSequence, Flatte
 
 from theano import tensor
 from blocks.initialization import IsotropicGaussian, Constant
-from Preprocessor.dataset_utils import PackerForFuel
-from Preprocessor.Base_utils import *
-from utilities import *
 
 sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)
 
+## Init the params.
+FRAME_NUM = 48
+FRAME_SIZE = 144
+IMAGE_SIZE = [FRAME_NUM, FRAME_SIZE]
 
-def main():
-    ## Init the params.
-    FRAME_NUM = 48
-    FRAME_SIZE = 144
-    IMAGE_SIZE = [FRAME_NUM, FRAME_SIZE]
+def prepare_data():
+    from Preprocessor.dataset_utils import PackerForFuel
+    from Preprocessor.Base_utils import *
+    from utilities import *
 
     # Prepare the data.
     print("Prepare the data.")
@@ -34,62 +34,85 @@ def main():
             SplitIntoBatches(FRAME_NUM,5),
             Monitor()]).run()
     cad60_train = PackerForFuel(data, label)
+    return
+
+class detector():
+    def __init__(self):
+        # Build the network.
+        print("Build the network")
+        x = tensor.tensor3('features')
+
+        x = x.reshape((x.shape[0], 1, IMAGE_SIZE[0], IMAGE_SIZE[1]))
+        # x = x.reshape((x.shape[0], 1, 1, x.shape[2]))
+
+        y = tensor.lmatrix('targets')
+
+        # Convolutional layers
+        filter_sizes = [(3, 3)] * 3
+        num_filters = [16, 32, 64]
+        pooling_sizes = [(3, 3)] * 3
+        activation = Sigmoid().apply
+        conv_layers = []
+
+        input_dims = list(IMAGE_SIZE)
+        for filter_size, num_filters_, pooling_size in zip(filter_sizes, num_filters, pooling_sizes):
+            try:
+                layer_count = layer_count+1
+            except:
+                layer_count = 0
+            new_conv_layer = ConvolutionalLayer(activation, filter_size, num_filters_, pooling_size)
+            new_conv_layer.name += str(layer_count)
+            conv_layers.append(new_conv_layer)
+
+        convnet = ConvolutionalSequence(conv_layers, num_channels=1,
+                                        image_size=tuple(IMAGE_SIZE),
+                                        weights_init=IsotropicGaussian(),
+                                        biases_init=Constant(0.))
+        convnet.initialize()
+
+        # Fully connected layers
+
+        features = Flattener().apply(convnet.apply(x))
+        mlp = MLP(activations=[Sigmoid(), Sigmoid(), Softmax()],
+                  dims=[320, 256, 100, 14], weights_init=IsotropicGaussian(),
+                  biases_init=Constant(0.))
+        mlp.initialize()
+
+        probs = mlp.apply(features)
+        load_params(probs, "params-after-train.pkl")
+        self.model = probs
+
+    def detect(self, data):
+        suit_shape_of_x = lambda x:x.reshape(1,1,IMAGE_SIZE[0],IMAGE_SIZE[1])
+        result = self.model.eval({x:suit_shape_of_x(data)})[0]
+        result = list(result)
+        print result
+        return result.index(max(result))
 
 
-    # Build the network.
-    print("Build the network")
-    x = tensor.tensor3('features')
 
-    x = x.reshape((x.shape[0], 1, IMAGE_SIZE[0], IMAGE_SIZE[1]))
-    # x = x.reshape((x.shape[0], 1, 1, x.shape[2]))
+def test():
+    import zmq
+    import uuid
+    client = {}
+    context = zmq.Context()
+    socket = context.socket(zmq.REP)
+    socket.bind("tcp://*:5555")
+    print("begin to receive!")
+    while True:
+        new_data = socket.recv_pyobj()
+        for i in new_data:
+            try:
+                client[i] += [new_data[i]]
+            except:
+                client[i] = [] + [new_data[i]]
+            if len(client[i]) >=48 :
+                print len(client)
+        socket.send("ok!")
 
-    y = tensor.lmatrix('targets')
 
-    # Convolutional layers
-    filter_sizes = [(3, 3)] * 3
-    num_filters = [16, 32, 64]
-    pooling_sizes = [(3, 3)] * 3
-    activation = Sigmoid().apply
-    conv_layers = []
-
-    input_dims = list(IMAGE_SIZE)
-    for filter_size, num_filters_, pooling_size in zip(filter_sizes, num_filters, pooling_sizes):
-        try:
-            layer_count = layer_count+1
-        except:
-            layer_count = 0
-        new_conv_layer = ConvolutionalLayer(activation, filter_size, num_filters_, pooling_size)
-        new_conv_layer.name += str(layer_count)
-        conv_layers.append(new_conv_layer)
-
-    convnet = ConvolutionalSequence(conv_layers, num_channels=1,
-                                    image_size=tuple(IMAGE_SIZE),
-                                    weights_init=IsotropicGaussian(),
-                                    biases_init=Constant(0.))
-    convnet.initialize()
-
-    # Fully connected layers
-
-    features = Flattener().apply(convnet.apply(x))
-    # features = x.flatten()
-    mlp = MLP(activations=[Sigmoid(), Sigmoid(), Softmax()],
-              dims=[320, 256, 100, 14], weights_init=IsotropicGaussian(),
-              biases_init=Constant(0.))
-    mlp.initialize()
-
-    probs = mlp.apply(features)
-    load_params(probs, "params-after-train.pkl")
-    example_stream = cad60_train.get_example_stream()
-    iter_of_epochs = example_stream.iterate_epochs()
-    data_of_epoch = iter_of_epochs.next()
-    data_point = data_of_epoch.next()
-    suit_shape_of_x = lambda x:x.reshape(1,1,IMAGE_SIZE[0],IMAGE_SIZE[1])
-    print data_point
-    result = probs.eval({x:suit_shape_of_x(data_point[0])})[0]
-    result = list(result)
-    print result.index(max(result))
 
 
 if __name__ == "__main__":
-    main()
+    test()
 
