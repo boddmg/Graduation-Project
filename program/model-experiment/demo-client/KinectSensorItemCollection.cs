93d5342700c169fcaf2035a532a0f3f072ea633b
boddmg <boddmg@gmail.com>
2015-06-12 09:07:49 +0800

    1.Change the cad60-cnn dir to the model-experiment. 2.Add the demo client.
    Signed-off-by: boddmg <boddmg@gmail.com>

---
+++ program/model-experiment/demo-client/KinectSensorItemCollection.cs
@@ -0,0 +1,52 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="KinectSensorItemCollection.cs" company="Microsoft">
+//     Copyright (c) Microsoft Corporation.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+namespace Microsoft.Samples.Kinect.KinectExplorer
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using Microsoft.Kinect;
+
+    /// <summary>
+    /// An ObservableCollection of KinectSensorItems, used to track collection changes.
+    /// </summary>
+    public class KinectSensorItemCollection : ObservableCollection<KinectSensorItem>
+    {
+        private readonly Dictionary<KinectSensor, KinectSensorItem> sensorLookup = new Dictionary<KinectSensor, KinectSensorItem>();
+
+        public Dictionary<KinectSensor, KinectSensorItem> SensorLookup
+        {
+            get
+            {
+                return this.sensorLookup;
+            }
+        }
+
+        protected override void InsertItem(int index, KinectSensorItem item)
+        {
+            if (item == null)
+            {
+                throw new ArgumentException("Inserted item can't be null.", "item");
+            }
+
+            this.SensorLookup.Add(item.Sensor, item);
+            base.InsertItem(index, item);
+        }
+
+        protected override void RemoveItem(int index)
+        {
+            this.SensorLookup.Remove(this[index].Sensor);
+            base.RemoveItem(index);
+        }
+
+        protected override void ClearItems()
+        {
+            this.SensorLookup.Clear();
+            base.ClearItems();
+        }
+    }
+}
---
+++ program/model-experiment/demo-client/KinectWindow.xaml.cs
@@ -0,0 +1,128 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="KinectWindow.xaml.cs" company="Microsoft">
+//     Copyright (c) Microsoft Corporation.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+namespace Microsoft.Samples.Kinect.KinectExplorer
+{
+    using System.Diagnostics.CodeAnalysis;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Data;
+    using System.Windows.Input;
+    using Microsoft.Kinect;
+    using Microsoft.Samples.Kinect.WpfViewers;
+
+    /// <summary>
+    /// Interaction logic for KinectWindow.xaml.
+    /// </summary>
+    public partial class KinectWindow : Window
+    {
+        public static readonly DependencyProperty KinectSensorProperty =
+            DependencyProperty.Register(
+                "KinectSensor",
+                typeof(KinectSensor),
+                typeof(KinectWindow),
+                new PropertyMetadata(null));
+
+        private readonly KinectWindowViewModel viewModel;
+
+        /// <summary>
+        /// Initializes a new instance of the KinectWindow class, which provides access to many KinectSensor settings
+        /// and output visualization.
+        /// </summary>
+        public KinectWindow()
+        {
+            this.viewModel = new KinectWindowViewModel();
+
+            // The KinectSensorManager class is a wrapper for a KinectSensor that adds
+            // state logic and property change/binding/etc support, and is the data model
+            // for KinectDiagnosticViewer.
+            this.viewModel.KinectSensorManager = new KinectSensorManager();
+
+            Binding sensorBinding = new Binding("KinectSensor");
+            sensorBinding.Source = this;
+            BindingOperations.SetBinding(this.viewModel.KinectSensorManager, KinectSensorManager.KinectSensorProperty, sensorBinding);
+
+            // Attempt to turn on Skeleton Tracking for each Kinect Sensor
+            this.viewModel.KinectSensorManager.SkeletonStreamEnabled = true;
+
+            this.DataContext = this.viewModel;
+            
+            InitializeComponent();
+        }
+
+        public KinectSensor KinectSensor
+        {
+            get { return (KinectSensor)GetValue(KinectSensorProperty); }
+            set { SetValue(KinectSensorProperty, value); }
+        }
+
+        public void StatusChanged(KinectStatus status)
+        {
+            this.viewModel.KinectSensorManager.KinectSensorStatus = status;
+        }
+
+        private void Swap_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            Grid colorFrom = null;
+            Grid depthFrom = null;
+
+            if (this.MainViewerHost.Children.Contains(this.ColorVis))
+            {
+                colorFrom = this.MainViewerHost;
+                depthFrom = this.SideViewerHost;
+            }
+            else
+            {
+                colorFrom = this.SideViewerHost;
+                depthFrom = this.MainViewerHost;
+            }
+
+            colorFrom.Children.Remove(this.ColorVis);
+            depthFrom.Children.Remove(this.DepthVis);
+            colorFrom.Children.Insert(0, this.DepthVis);
+            depthFrom.Children.Insert(0, this.ColorVis);
+        }
+    }
+
+    /// <summary>
+    /// A ViewModel for a KinectWindow.
+    /// </summary>
+    public class KinectWindowViewModel : DependencyObject
+    {
+        public static readonly DependencyProperty KinectSensorManagerProperty =
+            DependencyProperty.Register(
+                "KinectSensorManager",
+                typeof(KinectSensorManager),
+                typeof(KinectWindowViewModel),
+                new PropertyMetadata(null));
+
+        public static readonly DependencyProperty DepthTreatmentProperty =
+            DependencyProperty.Register(
+                "DepthTreatment",
+                typeof(KinectDepthTreatment),
+                typeof(KinectWindowViewModel),
+                new PropertyMetadata(KinectDepthTreatment.ClampUnreliableDepths));
+
+        public KinectSensorManager KinectSensorManager
+        {
+            get { return (KinectSensorManager)GetValue(KinectSensorManagerProperty); }
+            set { SetValue(KinectSensorManagerProperty, value); }
+        }
+
+        public KinectDepthTreatment DepthTreatment
+        {
+            get { return (KinectDepthTreatment)GetValue(DepthTreatmentProperty); }
+            set { SetValue(DepthTreatmentProperty, value); }
+        }
+    }
+
+    /// <summary>
+    /// The Command to swap the viewer in the main panel with the viewer in the side panel.
+    /// </summary>
+    public class KinectWindowsViewerSwapCommand : RoutedCommand
+    {  
+    }
+}
